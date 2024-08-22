d3ec5cc8e4c05bc22b284f21f835cb75ceaacf08
RehmanAkram <rehmanakram03@gmail.com>
2024-08-22 14:46:29 +0500

    updated app module and connectivity with mongo db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import configurations from './config/configurations';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_URI'),
+      }),
+      inject: [ConfigService],
+    }),
+
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configurations],
+      cache: true,
+    }),
+  ],
 })
 export class AppModule {}
