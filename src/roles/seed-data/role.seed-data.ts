56c32245ba3cf9ed51ad726ac2627b6148d27a95
RehmanAkram <rehmanakram03@gmail.com>
2024-08-23 11:21:07 +0500

    updated role seeders

--- src/roles/seed-data/role.seed-data.ts
+++ src/roles/seed-data/role.seed-data.ts
@@ -5,4 +5,12 @@ export const rolesSeedData = [
     name: DEFAULT_ROLES.SUPER_ADMIN,
     description: 'Super Administrator',
   },
+  {
+    name: DEFAULT_ROLES.USER,
+    description: 'Simple user role',
+  },
+  {
+    name: DEFAULT_ROLES.ADMIN,
+    description: 'Admin role',
+  },
 ];
